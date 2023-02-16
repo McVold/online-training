48451be6052c536241a15ad42c9517f8a65e263b
Vladimir Myagkov <119622825+McVold@users.noreply.github.com>
2023-02-16 13:55:11 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,20 @@
+var exampleModal = document.getElementById('exampleModal')
+exampleModal.addEventListener('show.bs.modal', function (event) {
+  // Кнопка, запускающая модальное окно
+  var button = event.relatedTarget
+  // Извлечь информацию из атрибутов data-bs- *
+  var recipient = button.getAttribute('data-bs-whatever')
+  // При необходимости вы можете инициировать запрос AJAX здесь
+  // а затем выполните обновление в обратном вызове.
+  //
+  // Обновите содержимое модального окна.
+  var modalTitle = exampleModal.querySelector('.modal-title')
+  var modalBodyInput = exampleModal.querySelector('.modal-body input')
+
+  modalTitle.textContent = recipient
+});
+
+document.querySelector('.first-button').addEventListener('click', function () {
+
+  document.querySelector('.animated-icon1').classList.toggle('open');
+});
